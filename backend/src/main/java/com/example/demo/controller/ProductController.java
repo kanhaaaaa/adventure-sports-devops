a64a45ce3efb2dedd3cb1a19d5ece83d958b9cfa
Prathmesh <kanhasundaram06@gmail.com>
2026-04-15 13:27:25 +0530

    updated backend and frontend

--- backend/src/main/java/com/example/demo/controller/ProductController.java
+++ backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import com.example.demo.service.ProductService;
 
 
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class ProductController {
 
